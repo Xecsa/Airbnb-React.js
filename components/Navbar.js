b75048ebec595e47f78ba9ef98c852af0519476d
Saba <xecsaba@gmail.com>
2023-08-07 21:40:55 +0400

    Airbnb Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,8 +3,7 @@ import React from "react"
 export default function Navbar(){
     return(
         <nav>
-            <img src="../images/airbnb1.png" />
-
+            <img src="../images/airbnb1.png" className="nav--logo" />
         </nav>
     )
 }
