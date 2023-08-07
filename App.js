97884c2cc66e3609cf9a73f26246d428a6a0fd40
Saba <xecsaba@gmail.com>
2023-08-07 22:35:54 +0400

    Hero Component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
 
 /* 
 Challenge:  Build the Navbar component. 
@@ -9,7 +10,12 @@ Check the Figma file for the design specifics.
 export default function App() {
     return (
         <div>
-            <Navbar />
+                <Navbar />
+                < Hero />
+            
         </div>
+        
+        
+        
     )
 }
